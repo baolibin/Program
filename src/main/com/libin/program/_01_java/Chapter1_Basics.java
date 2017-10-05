819ea21c1ae8134ea3fa83470b4da825f0e8ccac
baolibin <2578858653@qq.com>
2017-10-05 20:30:52 +0800

    demo

---
+++ src/main/com/libin/program/_01_java/Chapter1_Basics.java
@@ -0,0 +1,7 @@
+package libin.program._01_java;
+
+public class Chapter1_Basics {
+    public static void main(String[] args){
+        System.out.println("hello java");
+    }
+}
--- src/main/com/libin/program/_02_scala/Chapter1_Basics.scala
+++ src/main/com/libin/program/_02_scala/Chapter1_Basics.scala
@@ -2,6 +2,6 @@ package libin.program._02_scala
 
 object Chapter1_Basics {
   def main(args: Array[String]): Unit = {
-
+    println("hello scala")
   }
 }
