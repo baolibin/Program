d236bd700fff498319c3bfebaa79639a834908a5
baolibin <2578858653@qq.com>
2017-10-07 19:29:17 +0800

    Scala基础语法

--- src/main/com/libin/program/_02_scala/Chapter1_Basics.scala
+++ src/main/com/libin/program/_02_scala/Chapter1_Basics.scala
@@ -1,7 +1,12 @@
 package libin.program._02_scala
 
+/**
+  * Scala一些基础的认知
+  */
 object Chapter1_Basics {
   def main(args: Array[String]): Unit = {
-    println("hello scala")
+    //定义变量和常量
+    val v1: String = "hello world"
+    var v2: String = "hello world"
   }
 }
