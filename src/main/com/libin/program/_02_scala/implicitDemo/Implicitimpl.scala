98dc3d4853807205073b7b60687f07d25fcac32c
baolibin <baolibin@qq.com>
2018-09-09 23:37:26 +0800

    scala implicit

---
+++ src/main/com/libin/program/_02_scala/implicitDemo/Implicitimpl.scala
@@ -0,0 +1,20 @@
+package main.com.libin.program._02_scala.implicitDemo
+
+import main.com.libin.program._02_scala.implicitDemo.ImplicitBase.MyPerson
+
+
+/**
+  * Copyright (c) 2018/9/9. xixi Inc. All Rights Reserved.
+  * Authors: libin <2578858653@qq.com>
+  * <p>
+  * Purpose : 
+  */
+object Implicitimpl {
+  def main(args: Array[String]): Unit = {
+
+    val myPerson = MyPerson("libin", 18)
+    myPerson.mySay()
+
+  }
+
+}
