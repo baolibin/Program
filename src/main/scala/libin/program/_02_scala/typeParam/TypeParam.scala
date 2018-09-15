894b27a8c69fc47df270c8188571260cba5145dd
baolibin <2578858653@qq.com>
2018-09-15 23:40:14 +0800

    scala 上界

--- src/main/scala/libin/program/_02_scala/typeParam/TypeParam.scala
+++ src/main/scala/libin/program/_02_scala/typeParam/TypeParam.scala
@@ -8,12 +8,22 @@ package libin.program._02_scala.typeParam
   */
 object TypeParam {
   def main(args: Array[String]): Unit = {
-    val p = new pair("hadoop", "spark")
-    println(p.getSmall)
+    val p1 = new pair("hadoop", "spark")
+    println(p1.getSmall)
+    val p2 = new PairDown("hadoop", "spark")
+    println(p2.replaceFirst("hive").toString)
   }
 }
 
 // 上界
 class pair[T <: Comparable[T]](val first: T, val second: T) {
   def getSmall: T = if (first.compareTo(second) < 0) first else second
+}
+
+// 下界
+class PairDown[T](val first: T, val second: T) {
+
+  def replaceFirst[R >: T](newFirst: R) = new PairDown[R](newFirst, second)
+
+  override def toString: String = this.first + "\t" + this.second
 }
