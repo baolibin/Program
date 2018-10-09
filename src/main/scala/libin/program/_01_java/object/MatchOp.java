5d6e577d20cdb9d89838801fcf22c338741cfe25
baolibin <2578858653@qq.com>
2018-10-09 23:50:42 +0800

    match

---
+++ src/main/scala/libin/program/_01_java/object/MatchOp.java
@@ -0,0 +1,17 @@
+package libin.program._01_java.object;
+
+
+/**
+ * Copyright (c) 2018/10/09. xixi Inc. All Rights Reserved.
+ * Authors: libin <2578858653@qq.com>
+ * <p>
+ * Purpose :
+ */
+public class MatchOp {
+    public static void main(String[] args){
+
+        // 自然常数e为底的指数函数
+        System.out.println(Math.exp(1));
+
+    }
+}
