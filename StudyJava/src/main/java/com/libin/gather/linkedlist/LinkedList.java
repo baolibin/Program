de8427716c2de73b3b10fc267d45f6a40543d5f0
baolibin <2578858653@qq.com>
2020-03-07 17:29:34 +0800

    linkedlist 初始化、添加、删除

--- StudyJava/src/main/java/com/libin/gather/linkedlist/LinkedList.java
+++ StudyJava/src/main/java/com/libin/gather/linkedlist/LinkedList.java
@@ -4,7 +4,8 @@ package com.libin.gather.linkedlist;
  * Copyright (c) 2020/03/06. xixi Inc. All Rights Reserved.
  * Authors: libin <2578858653@qq.com>
  * <p>
- * Purpose :
+ * Purpose : JDK1.8
+ * LinkedList底层是使用双向链表实现的，线程不安全。
  */
 public class LinkedList<E> extends java.util.LinkedList<E> {
 }
