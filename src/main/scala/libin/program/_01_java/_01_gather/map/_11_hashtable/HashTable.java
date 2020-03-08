e6abb64939bd17ddb36398d006c03ae53b7bf96d
baolibin <2578858653@qq.com>
2020-03-08 19:23:20 +0800

    初始化map相关集合类

---
+++ src/main/scala/libin/program/_01_java/_01_gather/map/_11_hashtable/HashTable.java
@@ -0,0 +1,10 @@
+package libin.program._01_java._01_gather.map._11_hashtable;
+
+/**
+ * Copyright (c) 2020/03/08. xixi Inc. All Rights Reserved.
+ * Authors: libin <2578858653@qq.com>
+ * <p>
+ * Purpose :
+ */
+public class HashTable {
+}
