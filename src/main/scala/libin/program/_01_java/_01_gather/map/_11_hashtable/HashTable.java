a8371b8f737d2d4b3f7fab1f7761031089febda4
baolibin <2578858653@qq.com>
2020-03-20 20:59:06 +0800

    sql leetcode

--- src/main/scala/libin/program/_01_java/_01_gather/map/_11_hashtable/HashTable.java
+++ src/main/scala/libin/program/_01_java/_01_gather/map/_11_hashtable/HashTable.java
@@ -1,6 +1,7 @@
 package libin.program._01_java._01_gather.map._11_hashtable;
 
 import java.util.Hashtable;
+import java.util.Queue;
 
 /**
  * Copyright (c) 2020/03/08. xixi Inc. All Rights Reserved.
@@ -9,4 +10,8 @@ import java.util.Hashtable;
  * Purpose :
  */
 public class HashTable<K, V> extends Hashtable<K, V> {
+    public static void main(String[] args){
+        int[] ints = new int[128];
+        char[] chars = "asd".toCharArray();
+    }
 }
